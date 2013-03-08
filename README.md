eeef04476b20224859e5af82b3a25db477e94c8d
Peter Flynn <pflynn@adobe.com>
2013-03-07 16:42:26 -0800

    Initial checkin. Works for core & extension modules selecting require() vs. brackets.getModule() as appropriate. Does NOT match whitespace of other require() lines yet.

---
+++ README.md
@@ -0,0 +1,35 @@
+Quick Import
+============
+Quick Import is a shortcut for generating RequireJS/CommonJS import statements like this:
+
+```
+        Commands = require("command/Commands"),
+```
+
+Press Ctrl+I to open a file-search popup, find the file you want, and hit Enter &ndash; an import line for the file is inserted
+on the line beneath your cursor. Also supports brackets.getModule(), if you have a src/extensions folder tree.
+
+### Caveats
+* Your cursor must be inside the block of require() calls where you want the new code to be inserted
+* Assumes the root of your require() source tree is a folder named "src"
+* Assumes this declaration is part of a comma-separated list of imports &ndash; that is, assumes there's a `var` line above it
+  and a line ending in `;` below it, so the line you get ends in `,`
+
+
+What is Brackets?
+=================
+Quick Import is an extension for [Brackets](https://github.com/adobe/brackets/), a new open-source code editor for the web.
+
+To use Quick Import:
+
+1. [Download the ZIP](https://github.com/peterflynn/quick-import/archive/master.zip) and unzip it; or clone this repo
+2. Open your extensions folder: _Help > Show Extensions Folder_
+3. Place the folder so the structure is: `extensions/user/quick-import/main.js`
+4. Restart Brackets!
+
+
+### License
+MIT-licensed -- see `main.js` for details.
+
+### Compatibility
+Brackets Sprint 22 or newer (not compatible with Adobe Edge Code yet).
---
+++ main.js
@@ -0,0 +1,302 @@
+/*
+ * Copyright (c) 2012 Peter Flynn.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    // Brackets modules
+    var CommandManager      = brackets.getModule("command/CommandManager"),
+        Commands            = brackets.getModule("command/Commands"),
+        Menus               = brackets.getModule("command/Menus"),
+        ProjectManager      = brackets.getModule("project/ProjectManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        FileIndexManager    = brackets.getModule("project/FileIndexManager"),
+        QuickOpen           = brackets.getModule("search/QuickOpen");
+    
+    // TODO
+    //  - detect equal sign spacing
+    //  - detect neighboring brackets.getModule()
+    //  - allow invoking from anywhere in file
+    //  - bump out other lines' = signs if needed
+    //  - if first line, add 'var'
+    //  - if last line, move ';' down
+    //  - if text selected, replace instead of insert
+    //  - if file extension != .js, generate a "text!" require() instead
+    //  - detect indent level
+    //  - detect duplicates
+    
+    
+    // --------------------------------------------------------------------------------------------
+    // Code generation & insertion
+    
+    var REQUIRE_ROOT = "/src/";
+    var EXTENSIONS_ROOT = "extensions/";  // atop REQUIRE_ROOT
+    
+    function stripPrefix(str, prefix, allowMoreToLeft) {
+        var index = str.indexOf(prefix);
+        if (index === -1 || (!allowMoreToLeft && index !== 0)) {
+            return null;
+        }
+        return str.substring(index + prefix.length);
+    }
+    function afterLast(str, prefix) {
+        var index = str.lastIndexOf(prefix);
+        if (index === -1) {
+            return str;
+        }
+        return str.substring(index + 1);
+    }
+    function beforeLast(str, prefix) {
+        var index = str.lastIndexOf(prefix);
+        if (index === -1) {
+            return str;
+        }
+        return str.substring(0, index);
+    }
+    
+    /**
+     * Given a path relative to REQUIRE_ROOT. Returns:
+     *    requirePath: string passed to require() - root-relative path sans file extension
+     *    moduleName: file name sans extension alone
+     *    extensionName: name of extension folder requirePath is relative to, if any
+     */
+    function parseModulePath(fullPath) {
+        var relPath = stripPrefix(fullPath, REQUIRE_ROOT, true);
+        if (!relPath) {
+            console.error("File lies outside Require root: " + fullPath);
+            return;
+        }
+        
+        // Is this module in an extension?
+        var extensionName;
+        var extensionsRootRelPath = stripPrefix(relPath, EXTENSIONS_ROOT);
+        if (extensionsRootRelPath) {
+            extensionName = extensionsRootRelPath.substring(0, extensionsRootRelPath.indexOf("/")); // will be "" if -1
+            if (!extensionName) {
+                console.error("File is in extensions root but not in any extension: " + fullPath);
+                return;
+            }
+            
+            // Change relPath's context from global Require root to this extenion's Require root
+            relPath = extensionsRootRelPath.substring(extensionName.length + 1);
+        }
+        
+        var fileName = afterLast(relPath, "/");
+        return {
+            requirePath: beforeLast(relPath, "."),
+            moduleName: beforeLast(fileName, "."),
+            extensionName: extensionName
+        };
+    }
+    
+    /**
+     * @param {SearchResult} selectedItem
+     */
+    function itemSelect(selectedItem) {
+        
+        // Generate import code
+        var editor = EditorManager.getActiveEditor();
+        if (selectedItem && editor) {
+            
+            // If...
+            // Import is in extension, file being edited isn't -- error
+            // Import is in extension, file being edited is too -- use require() (but if *different* extensions, error)
+            // Import not in extension, file being edit isn't either -- use require()
+            // Import not in extension, file being edit is -- use brackets.getModule()
+            
+            var importPath  = selectedItem.fullPath;
+            var currentPath = editor.document.file.fullPath;
+            var importModuleInfo  = parseModulePath(importPath);
+            var currentModuleInfo = parseModulePath(currentPath);
+            
+            var requireCall;
+            if (importModuleInfo.extensionName) {
+                if (!currentModuleInfo.extensionName) {
+                    console.error("Can't import extension file " + importPath + " from outside module " + currentPath);
+                } else if (currentModuleInfo.extensionName !== importModuleInfo.extensionName) {
+                    console.error("Can't import extension file " + importPath + " from different extension's module " + currentPath);
+                } else {
+                    requireCall = "require";
+                }
+            } else {
+                if (currentModuleInfo.extensionName) {
+                    requireCall = "brackets.getModule";
+                } else {
+                    requireCall = "require";
+                }
+            }
+            if (!requireCall) {
+                return;
+            }
+            
+            var code = "        " + importModuleInfo.moduleName + " = " + requireCall + "(\"" + importModuleInfo.requirePath + "\"),\n";
+            
+            var insertionPos = editor.getCursorPos();
+            insertionPos.line++;
+            insertionPos.ch = 0;
+            editor.document.replaceRange(code, insertionPos);
+        }
+    }
+    
+    
+    // --------------------------------------------------------------------------------------------
+    // File search UI similar to Quick Open's default mode
+    
+    var searchPromise;
+    var latestQuery;
+    
+    /**
+     * @param {!string} query
+     * @param {!Array.<FileInfo>} fileList
+     * @param {!StringMatcher} matcher
+     */
+    function doSearch(query, fileList, matcher) {
+        query = query.substr(1);  // lose the "=" prefix
+        
+        // TODO: this part copied from QuickOpen.searchFileList()
+        // First pass: filter based on search string; convert to SearchResults containing extra info
+        // for sorting & display
+        var filteredList = $.map(fileList, function (fileInfo) {
+            // Is it a match at all?
+            // match query against the full path (with gaps between query characters allowed)
+            var searchResult = matcher.match(ProjectManager.makeProjectRelativeIfPossible(fileInfo.fullPath), query);
+            if (searchResult) {
+                searchResult.label = fileInfo.name;
+                searchResult.fullPath = fileInfo.fullPath;
+            }
+            return searchResult;
+        });
+        
+        // Sort by "match goodness" tier first; break ties alphabetically by short filename
+        QuickOpen.basicMatchSort(filteredList);
+        
+        return filteredList;
+    }
+    
+    /**
+     * @param {string} query User query/filter string
+     * @return {Array.<SearchResult>|$.Promise} Sorted and filtered results that match the query, or a promise
+     *      resolved with such an array later.
+     */
+    function search(query, matcher) {
+        // We're useless if there's no file open to insert text into
+        if (!EditorManager.getActiveEditor()) {
+            return [];
+        }
+        
+        // We're already async waiting on files list, nothing more we can do yet
+        if (searchPromise) {
+            latestQuery = query;
+            return searchPromise;
+        }
+        
+        var fileList;
+        var fileListPromise = FileIndexManager.getFileInfoList("all")
+            .done(function (result) {
+                fileList = result;
+            });
+        
+        if (fileListPromise.isResolved()) {
+            return doSearch(query, fileList, matcher);
+        } else {
+            // Index isn't built yet - start waiting
+            latestQuery = query;
+            searchPromise = new $.Deferred();
+            fileListPromise.done(function () {
+                searchPromise.resolve(doSearch(latestQuery, fileList, matcher));
+                searchPromise = null;
+                latestQuery = null;
+            });
+            return searchPromise.promise();
+        }
+    }
+    
+    /**
+     * @param {SearchResult} fileEntry
+     * @param {string} query
+     * @return {string}
+     */
+    function resultFormatter(item, query) {
+        // TODO: copied from QuickOpen._filenameResultsFormatter()
+        
+        // For main label, we just want filename: drop most of the string
+        function fileNameFilter(includesLastSegment, rangeText) {
+            if (includesLastSegment) {
+                var rightmostSlash = rangeText.lastIndexOf('/');
+                return rangeText.substring(rightmostSlash + 1);  // safe even if rightmostSlash is -1
+            } else {
+                return "";
+            }
+        }
+        var displayName = QuickOpen.highlightMatch(item, null, fileNameFilter);
+        var displayPath = QuickOpen.highlightMatch(item, "quicksearch-pathmatch");
+        
+        return "<li>" + displayName + "<br /><span class='quick-open-path'>" + displayPath + "</span></li>";
+    }
+    
+    /**
+     * @param {string} query
+     * @return {boolean} true if this plugin wants to provide results for this query
+     */
+    function match(query) {
+        if (query.indexOf("=") === 0) {
+            return true;
+        }
+    }
+    
+    
+    // Register as a new Quick Open mode
+    QuickOpen.addQuickOpenPlugin(
+        {
+            name: "Quick RequireJS Import",
+            label: "RequireJS Import",  // ignored before Sprint 22
+            fileTypes: [],  // empty array = all file types
+            done: function () {},
+            search: search,
+            match: match,
+            itemFocus: function () {},
+            itemSelect: itemSelect,
+            resultsFormatter: resultFormatter
+        }
+    );
+    
+    function beginFileSearch() {
+        // Begin Quick Open in our search mode
+        QuickOpen.beginSearch("=");
+    }
+    
+    
+    // Command to launch our Quick Open mode
+    var QUICK_IMPORT_COMMAND_ID = "pflynn.quickRequireImport";
+    CommandManager.register("RequireJS Import", QUICK_IMPORT_COMMAND_ID, beginFileSearch);
+    
+    // Add menu item too
+    var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+    menu.addMenuDivider(Menus.LAST);
+    menu.addMenuItem(QUICK_IMPORT_COMMAND_ID, "Ctrl-I", Menus.LAST);
+    
+});
